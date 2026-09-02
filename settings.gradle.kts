fd80a52df04de9e39fc1fd275c30fa18ca0e1c97
Chris Barry <chris.barry@makemoredigital.com>
2026-09-02 17:37:10 +0100

    Use local mobile agent subrepos

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.library") version "8.9.1"
-        id("com.android.application") version "8.9.1"
+        id("com.android.library") version "9.0.1"
+        id("com.android.application") version "9.0.1"
         id("org.jetbrains.kotlin.android") version "2.1.0"
         id("org.jetbrains.kotlin.plugin.compose") version "2.1.0"
         id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
